dba3fe7fa67c971202636f93d23698977ed76a18
De Haven Prillerman <dprillerman@gmail.com>
2013-08-11 22:01:06 -0400

    Initial Commit

---
+++ gituseexample/DAPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAPAppDelegate.h
+//  gituseexample
+//
+//  Created by De Haven Prillerman on 8/11/13.
+//  Copyright (c) 2013 De Haven Prillerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gituseexample/DAPFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  DAPFirstViewController.h
+//  gituseexample
+//
+//  Created by De Haven Prillerman on 8/11/13.
+//  Copyright (c) 2013 De Haven Prillerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAPFirstViewController : UIViewController
+
+@end
